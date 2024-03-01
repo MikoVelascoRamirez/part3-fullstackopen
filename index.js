c589d69ad398e1dd83767c6c98fdf9a75c3fb403
Miko Velasco Ramirez <launchx08269@innovaccion.mx>
2024-02-29 22:04:34 -0600

    2. Migrating files to own repo..

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/notes", (req, res) => {
+  const body = req.body;
+
+  if (!body.content) {
+    return res.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId()
+  }
+
+  notes = notes.concat(note);
+
+  // console.log(note, maxId)
+  res.json(note);
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello CHUECK</h1>");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find((note) => note.id === id);
+
+  if (note) res.json(note);
+  else res.status(404).end();
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  notes = notes.filter((note) => note.id !== id);
+  res.status(204).end();
+});
+
+app.listen(PORT, () => console.log(`server listening on PORT ${PORT}`));
